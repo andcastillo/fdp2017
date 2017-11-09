2739720dfe95e2f3f4c5f9d5d9bea313190780d7
DFGonzalez <diegofern.88@gmail.com>
2017-11-09 17:11:46 -0500

    Implementacion de Nodo, Seq-scan, RemoveRepeated

--- IOperator.java
+++ IOperator.java
@@ -1,5 +1,5 @@
 interface IOperator {
-    public String setOperatorName(String name);
+    public void setOperatorName(String name);
     public String getOperatorName();
     public String apply(Node node);
 }
