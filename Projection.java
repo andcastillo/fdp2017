9a14dc290849afff1dcaa33d6f429adf88714a39
wilmermunoz <wilmer.munoz@correounivalle.edu.co>
2017-11-14 10:10:22 -0500

    Test Proyeccion

--- Projection.java
+++ Projection.java
@@ -19,19 +19,19 @@ import java.util.logging.Logger;
  */
 public class Projection implements IOperator {
 
-   String type;
-	Integer items ;
+    String type;
+    Integer items ;
 
 
-	@Override
-	public void setOperatorName(String name) {
-		this.type = name;
-	}
+    @Override
+    public void setOperatorName(String name) {
+        this.type = name;
+    }
 
-	@Override
-	public String getOperatorName() {
-		return type;
-	}
+    @Override
+    public String getOperatorName() {
+        return type;
+    }
 
     @Override
     public String apply(Node node) {
@@ -50,7 +50,7 @@ public class Projection implements IOperator {
             for (Object object: parts_schema){
                 String[] parts_s = String.valueOf(object).split("-");
                 columns.add(parts_s[0]);
-                }
+            }
 
             List<Integer> pos = new ArrayList<Integer>();
             for (Object object: attr){ pos.add(columns.indexOf(object));}
@@ -71,24 +71,24 @@ public class Projection implements IOperator {
             SeqScan scan = new SeqScan(tableName);
             items =0;
             while (scan.hasNext()) {
-                    List<Object> rowObject = (List<Object>) scan.next();
-                    try {
-                            String rowStr = "";
-                            for (Integer i: pos){
-                                rowStr += ','+rowObject.get(i).toString();
-                            }
-                            System.out.println("Pt:"+rowStr.substring(1));
-                            blockOutput.write(rowStr.substring(1));
-                            blockOutput.newLine();
-                        } catch (IOException e) {
-                                e.printStackTrace();
-                        }
-                   items++;
-                    if(items % limit == 0 && scan.hasNext()){
-                            blockCount++;
-                            close(blockOutput);
-                            blockOutput = createOutputFile(TableNameOutput, tableName, blockCount);
+                List<Object> rowObject = (List<Object>) scan.next();
+                try {
+                    String rowStr = "";
+                    for (Integer i: pos){
+                        rowStr += ','+rowObject.get(i).toString();
                     }
+                    System.out.println("Pt:"+rowStr.substring(1));
+                    blockOutput.write(rowStr.substring(1));
+                    blockOutput.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                items++;
+                if(items % limit == 0 && scan.hasNext()){
+                    blockCount++;
+                    close(blockOutput);
+                    blockOutput = createOutputFile(TableNameOutput, tableName, blockCount);
+                }
             }
             close(blockOutput);
             node.setTableNameOutput(TableNameOutput);
@@ -96,66 +96,66 @@ public class Projection implements IOperator {
 
 
         }
-       return null;
+        return null;
 
     }
 
     public static void writeschema(String dirStr, String tableName, String line) throws FileNotFoundException, IOException {
         File dir = new File("data/myDB/"+dirStr);
-		if(!dir.exists()){
-			dir.mkdirs();
-		}
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
         File fout = new File(dir+"/schema.txt");
-	FileOutputStream fos = new FileOutputStream(fout);
-	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
         bw.write(line);
         bw.newLine();
-	bw.close();
-}
+        bw.close();
+    }
 
 
     public BufferedWriter createOutputFile(String dirStr, String tableName, int block){
-		File dir = new File("data/myDB/"+dirStr);
-		if(!dir.exists()){
-			dir.mkdirs();
-		}
+        File dir = new File("data/myDB/"+dirStr);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
         try {
-    		File file = new File(dir+"/"+block+".csv");
-			return new BufferedWriter(new FileWriter(file));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            File file = new File(dir+"/"+block+".csv");
+            return new BufferedWriter(new FileWriter(file));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         return null;
 
-	}
+    }
 
-	public void close(BufferedWriter output){
-		if ( output != null ) {
+    public void close(BufferedWriter output){
+        if ( output != null ) {
             try {
-				output.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-          }
-	}
+                output.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-        public String openschema(String tableName){
-            String dataSchema = null;
-            File file = new File("data/myDB/"+tableName+"/schema.txt"); //Se carga el esquema
-		try {
-			Scanner inputStream = new Scanner(file);
-			while (inputStream.hasNext()) {
-				dataSchema = inputStream.nextLine();
-			}
-			inputStream.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-            return dataSchema;
+    public String openschema(String tableName){
+        String dataSchema = null;
+        File file = new File("data/myDB/"+tableName+"/schema.txt"); //Se carga el esquema
+        try {
+            Scanner inputStream = new Scanner(file);
+            while (inputStream.hasNext()) {
+                dataSchema = inputStream.nextLine();
+            }
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return dataSchema;
 
-	}
+    }
 
 
 }
