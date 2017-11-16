24711eb9ad142a84ec7378812c1f0c39fae28d59
DFGonzalez <diegofern.88@gmail.com>
2017-11-16 02:52:09 -0500

    Se actualiza la creacion de la base de datos y se crean unos printlns de ejemplo para mostrar como acceder a los datos de los esquemas cargados

--- Main.java
+++ Main.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 
 public class Main {
-
+	static Scanner scanner;
+	
 	public static void main(String[] args) {
 		System.out.println ("Consola SQL");
 		System.out.println ("Por favor introduzca una consulta:");
 		String input = "";
 
-		DataBase dataBase = new DataBase("A");
+		DataBase.initDataBase("myDB");
+		//Los siguientes prints en pantalla muestran como se usa el acceso a la bd
+		System.out.println("Como acceder a la base de datos:"+DataBase.getInstance().getSchemaMaps().size());
+		System.out.println("Como acceder a la base de datos:"+
+					DataBase.getInstance().getSchemaMaps().get("A"));//Obtenemos el esquema de A (Imprime Schema@4554617c puesto que es un objeto)
+		System.out.println("Como acceder a la base de datos:"+
+				DataBase.getInstance().getSchemaMaps().get("A").getTypes());//Obtenemos el esquema de A y luego los tipos de datos de A
 		
-		Scanner scanner;
+		scanner = new Scanner (System.in);
+		
 		boolean status = true;
 		while (status) {
-			scanner = new Scanner (System.in);
 			input = scanner.nextLine ();				//Recibe el texto ingresado por consola
 			if(input.equals("0")||input.equals("Exit")){//Dos opciones para salir digitando 0 o Exit
 				status = false;
 			}else{
-				dataBase.query(input);			//llamado a ejecutar consulta a la base de datos
+				DataBase.getInstance().query(input);			//llamado a ejecutar consulta a la base de datos
 			}
 			
 			System.out.println ("Consulta: \"" + input +"\"");
