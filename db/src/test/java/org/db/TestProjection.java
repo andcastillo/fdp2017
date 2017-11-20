3e1f1e7093eb3d627d1096de443a787a65340b29
DFGonzalez <diegofern.88@gmail.com>
2017-11-20 01:14:09 -0500

    Actualizacion TestProjection con clase DataBase

--- db/src/test/java/org/db/TestProjection.java
+++ db/src/test/java/org/db/TestProjection.java
@@ -1,12 +1,9 @@
 package org.db;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.db.core.DataBase;
 import org.db.core.Node;
 import org.db.operator.IOperator;
 import org.db.operator.Projection;
-import org.db.operator.RemoveRepeated;
 
 public class TestProjection {
 
@@ -14,6 +11,7 @@ public class TestProjection {
 
 
         // PROYECCION
+		DataBase.initDataBase("myDB");//Inicializar la base de datos para obtener los esquemas
         Node node = new Node();
 
         node.addTableInput("A");
@@ -23,17 +21,7 @@ public class TestProjection {
 
         IOperator op = new Projection();
         String rselt = op.apply(node);
-
-        // ELIMINACION DE REPETIDOS
-
-        Node node1 = new Node();
-        node1.addTableInput(rselt);
-
-        node1.setOperationName("RemoveRepeated");
-
-        IOperator op1 = new RemoveRepeated();
-        String table = op1.apply(node1);
-
+ 
         System.out.println("Salida: "+rselt);
     }
 
